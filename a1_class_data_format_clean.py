eebbd73b94afd4a2912e134006ddd89cfe437b4e
JordanEvan77 <jgropper@seattleu.edu>
2025-01-31 14:49:41 -0800

    setting up basic logic and columns of interest

--- a1_class_data_format_clean.py
+++ a1_class_data_format_clean.py
@@ -5,9 +5,72 @@
 #Regression tasks: Overall tier/LP?
 #Classification Tasks: Game to game victory, final tier placement
 
+# lets start with the binary task of game to game victory.
+
 #Exploratory Analysis
+def early_eda():
+    # How many nested columns are there?
 
+    # which ones seem interesting? check correlation
 
+    #which ones are too obvious, and have to do with data leak?
+
+    #which ones are most likely associated with early game decisions?
+    win_con = ['win']
+    columns = [
+        "12AssistStreakCount",
+        "HealFromMapSources",
+        "SWARM_DefeatMiniBosses",
+        "controlWardTimeCoverageInRiverOrEnemyHalf",
+        "dodgeSkillShotsSmallWindow",
+        "earlyLaningPhaseGoldExpAdvantage",
+        "effectiveHealAndShielding",
+        "enemyChampionImmobilizations",
+        "firstTurretKilledTime",
+        "goldPerMinute",
+        "immobilizeAndKillWithAlly",
+        "jungleCsBefore10Minutes",
+        "killParticipation",
+        "landSkillShotsEarlyGame",
+        "moreEnemyJungleThanOpponent",
+        "outerTurretExecutesBefore10Minutes",
+        "teamDamagePercentage",
+        "visionScorePerMinute",
+        "wardTakedownsBefore20M",
+        "assists",
+        "baronKills",
+        "deaths",
+        "damageDealtToObjectives",
+        "damageDealtToTurrets",
+        "damageSelfMitigated",
+        "dragonKills",
+        "firstBloodAssist",
+        "inhibitorTakedowns",
+        "neutralMinionsKilled",
+        "physicalDamageDealt",
+        "totalDamageDealt",
+        "totalHeal",
+        "totalTimeCCDealt",
+        "trueDamageDealt",
+        "visionScore",
+        "objectives",
+        "teamDamagePercentage",
+        "teamRiftHeraldKills",
+        "initialBuffCount",
+        "jungleCsBefore10Minutes",
+        "initialCrabCount",
+        "takedownsFirstXMinutes",
+        "killsOnLanersEarlyJungleAsJungler",
+        "killsOnOtherLanesEarlyJungleAsLaner",
+        "earlyLaningPhaseGoldExpAdvantage",
+        "laneMinionsFirst10Minutes",
+        "controlWardsPlaced",
+        "visionScorePerMinute",
+        "firstTurretKilledTime",
+        "turretPlatesTaken",
+        "abilityUses",
+        "landSkillShotsEarlyGame"
+    ]
 
 #Categorical Cleaning and Encoding
 # Encode final Ranking
---
+++ b1_reg_data_format_clean.py
@@ -0,0 +1,67 @@
+# This will again take some reshaping. We probably want some or average of the key variables?
+# Or something else that shows game to game improvement? We want to measure how someone can climb
+# and improve, so maybe difference between the min and max, to show improvment? Or difference
+# between average early on and average later on?
+
+
+#I will need to convert the tier, rank and LP into a single integer, like a sliding scale,
+# and merge that in from the all tier summoner ID lists I created. We don't have their initial rank,
+# just their final rank. would be interesting to take wins and losses to get games played,
+# and use that as an attribute too.
+
+def convert_rank_to_int(tier, rank, lp):
+    # Define the tier values
+    tier_values = {'Iron': 1000, 'Bronze': 2000, 'Silver': 3000, 'Gold': 4000, 'Platinum': 5000,
+        'Diamond': 6000}
+
+    # Define the rank values
+    rank_values = {'IV': 100, 'III': 200, 'II': 300, 'I': 400 #TODO: isn't there a rank 5?
+    }
+
+    # Calculate the final integer value
+    tier_value = tier_values[tier]
+    rank_value = rank_values[rank]
+    final_value = tier_value + rank_value + lp
+
+    return final_value
+
+
+final_rank = '' # will need to read in from original dataset and use funciton converter
+
+columns = [
+    "goldPerMinute",
+    "damagePerMinute",
+    "killParticipation",
+    "laneMinionsFirst10Minutes",
+    "champExperience",
+    "baronKills",
+    "dragonKills",
+    "turretTakedowns",
+    "inhibitorTakedowns",
+    "visionScore",
+    "controlWardsPlaced",
+    "wardsKilled",
+    "wardsPlaced",
+    "damageSelfMitigated",
+    "totalHeal",
+    "takedowns",
+    "deaths",
+    "champExperience",
+    "kills",
+    "longestTimeSpentLiving",
+    "riftHeraldTakedowns",
+    "inhibitorKills",
+    "totalDamageShieldedOnTeammates",
+    "totalTimeSpentDead",
+    "kda",
+    "allInPings",
+    "assistMePings",
+    "commandPings",
+    "dangerPings",
+    "onMyWayPings",
+    "basicPings",
+    "pushPings",
+    "visionClearedPings",
+    "skillshotsHit",
+    "neutralMinionsKilled"
+]
